b19023bee5279aeb5cfc4218d2712a08aa5f7281
= <=>
2022-05-25 14:19:55 -0700

    init

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions:[String] = [
+    "What is 7+7?",
+    "What is the capitol of Vermont?",
+    "What is cognac made from?"
+    ]
+    let anwswers:[String] = [
+    "14",
+    "Monteplier",
+    "Grapes"
+    ]
+    var currentQuestionIndex:Int = 0
+    
+    @IBAction func showNextQuestion(_sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex >= questions.count {
+            currentQuestionIndex = 0
+        }
+        let question:String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_sender: UIButton) {
+        let answer:String = anwswers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
 }
 
